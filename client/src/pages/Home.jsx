56def2d121fa4d8f6f8d0ce81e312fd6464ac70f
Fauzia Naava <fauzianaava@gmail.com>
2023-09-01 11:55:18 +0300

    Add fetch posts function

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,10 +13,51 @@ const RenderCards = ({ data, title }) => {
 };
 const Home = () => {
   const [loading, setLoading] = useState(false);
-  const [allPost, setAllPost] = useState(null);
+  const [allPosts, setAllPosts] = useState(null);
 
   const [searchText, setSearchText] = useState("");
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeOut, setSearchTimeOut] = useState(null);
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:8080/post", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reserve());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  });
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+
+    setSearchTimeOut(
+      setTimeout(() => {
+        const searchResults = allPosts.filter(
+          (item) =>
+            item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.prompt.toLowerCase().includes(searchText.toLowerCase())
+        );
+
+        setSearchedResults(searchResults);
+      }, 500)
+    );
+  };
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -29,7 +70,13 @@ const Home = () => {
         </p>
       </div>
       <div className="mt-16">
-        <FormField />
+        <FormField 
+        labelName="search posts"
+        type="text"
+        name="text"
+        placeholder="Search posts"
+        value={searchText}
+        handleChange={handleSearchChange}/>
       </div>
 
       <div className="mt-10">
@@ -47,12 +94,9 @@ const Home = () => {
             )}
             <div className="grid lg:grid-cols-4 sm:grid-col-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
-                <RenderCards
-                  data={[]}
-                  title="No search results found"
-                />
+                <RenderCards data={searchedResults} title="No search results found" />
               ) : (
-                <RenderCards data={[]} title="No posts found" />
+                <RenderCards data={[allPosts]} title="No posts found" />
               )}
             </div>
           </>
